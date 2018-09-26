52685439b5a0fa23f1b2a21c2d16ab3a13f166da
HOMMinteractive <43573733+HOMMinteractive@users.noreply.github.com>
2018-09-26 08:47:14 +0200

    Update Hommjuicer.php

--- src/Hommjuicer.php
+++ src/Hommjuicer.php
@@ -66,7 +66,7 @@
 			*
 			* @var string
 		*/
-		public $schemaVersion = '1.0.2';
+		public $schemaVersion = '1.0.3';
 		
 		// Public Methods
 		// =========================================================================
